9692aea94d9675ce583dd5a57e8f9c922ec397e6
jbudrow <jbudrowd3@yahoo.com>
2016-06-27 21:01:31 -0400

    sift through features spec to fix minor bug

--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.feature "Deleting an Article" do
 
   before do
-    @article = Article.create(title: "First article", body: "Body of first article")
+    john = User.create(email: "john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "First Article", body: "Body of first article", user: john)
   end
 
   scenario "A user deletes an article" do
@@ -13,6 +15,6 @@ RSpec.feature "Deleting an Article" do
     click_link "Delete Article"
 
     expect(page).to have_content("Article has been deleted")
-    expect(page.current_path).to eq(articles_path)
+    expect(current_path).to eq(articles_path)
   end
 end
--- spec/features/signing_users_in_spec.rb
+++ spec/features/signing_users_in_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 RSpec.feature "Users sign-in" do
 
   before do
-    @tycho = User.create!(email: "tycho@example.com", password: "password")
+    @john = User.create!(email: "john@example.com", password: "password")
   end
 
   scenario "with valid credentials" do
     visit "/"
 
     click_link "Sign in"
-    fill_in "Email", with: @tycho.email
-    fill_in "Password", with: @tycho.password
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
     click_button "Log in"
 
     expect(page).to have_content("Signed in successfully.")
-    expect(page).to have_content("Signed in as #{@tycho.email}")
+    expect(page).to have_content("Signed in as #{@john.email}")
   end
 end
